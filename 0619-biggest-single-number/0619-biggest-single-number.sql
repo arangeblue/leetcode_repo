56645cf2a68d39131b1800af993fafea0e6a1b70
arangeblue <80625452+arangeblue@users.noreply.github.com>
2024-07-05 18:45:54 +0900

    Time: 519 ms (53.29%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -2,8 +2,12 @@
 # 단일 숫자는 MyNumbers 테이블에 한 번만 나타나는 숫자입니다. 
 # 가장 큰 단일 숫자를 찾으십시오. 단일 숫자가 없으면 null을 보고합니다.
 
-select ifnull(max(num), null) as num
+select num
 from MyNumbers
 group by num
 having count(*) <= 1
+
+union all
+select null
+
 order by num desc limit 1
