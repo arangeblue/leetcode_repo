1055f6aa3a7e6520a7013da665cd6512d0cf5794
arangeblue <80625452+arangeblue@users.noreply.github.com>
2024-07-06 16:46:08 +0900

    Time: 584 ms (80.42%), Space: 0B (100%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -1,6 +1,7 @@
 # Write your MySQL query statement below
 # 첫 번째 문자만 대문자이고 나머지는 소문자로 이름을 수정하는 솔루션을 작성하세요. 
 # user_id순으로 정렬된 결과 테이블을 반환합니다.
+# 풀이 - substring(str, start, [length]), upper, lower, concat
 
 select user_id, concat(upper(substring(name,1, 1)),lower(substring(name,2))) as name
 from Users
