aaf0abd7ab11371291186d0427a512648a10d347
arangeblue <80625452+arangeblue@users.noreply.github.com>
2024-07-06 17:18:29 +0900

    Time: 400 ms (87.83%), Space: 0B (100%) - LeetHub

---
+++ 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+# 날짜별로 판매된 다양한 제품의 수와 이름을 찾는 솔루션을 작성하세요. 
+# 각 날짜에 판매된 제품 이름은 사전순으로 정렬되어야 합니다. 
+# Sell_date를 기준으로 정렬된 결과 테이블을 반환합니다.
+
+select sell_date, count(distinct product) as num_sold, group_concat(distinct product) as products
+from Activities
+group by sell_date
